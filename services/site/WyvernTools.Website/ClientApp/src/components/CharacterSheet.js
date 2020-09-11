9acc7ed6f8179134832546962dc8e915d6a5a607
aquartulli <alex.m.quartulli@gmail.com>
2020-09-10 21:35:17 -0400

    Init with character sheet start

---
+++ services/site/WyvernTools.Website/ClientApp/src/components/CharacterSheet.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+import { CharacterSheetAttribute } from './CharacterSheetAttribute';
+
+export class CharacterSheet extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            attributes: {
+                strength: 11,
+                dexterity: 12,
+                constitution: 7,
+                intelligence: 15,
+                wisdom: 3,
+                charisma: 13
+            }
+        };
+    }
+
+    render() {
+        return (
+            <div className="character-sheet-attributes-container">
+                <CharacterSheetAttribute name="Strength" value={this.state.attributes.strength} />
+                <CharacterSheetAttribute name="Dexterity" value={this.state.attributes.dexterity} />
+                <CharacterSheetAttribute name="Constitution" value={this.state.attributes.constitution} />
+                <CharacterSheetAttribute name="Intelligence" value={this.state.attributes.intelligence} />
+                <CharacterSheetAttribute name="Wisdom" value={this.state.attributes.wisdom} />
+                <CharacterSheetAttribute name="Charisma" value={this.state.attributes.charisma} />
+            </div>
+        );
+    }
+}
---
+++ services/site/WyvernTools.Website/ClientApp/src/components/CharacterSheetAttribute.js
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+
+export class CharacterSheetAttribute extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,
+            value: props.value
+        };
+    }
+
+    render() {
+        return (
+            <div className="character-sheet-attribute-container">
+                <div className="character-sheet-attribute-box">
+                    <div className="character-sheet-attribute-name">{this.state.name}</div>
+                    <div className="character-sheet-attribute-value">{this.state.value}</div>
+                </div>
+                <div className="character-sheet-modifier-box">{Math.floor((this.state.value - 10) / 2)}</div>
+            </div>
+        );
+    }
+}
